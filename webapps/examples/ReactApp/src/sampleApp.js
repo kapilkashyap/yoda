727b5c391a8e6fc4caf10ae54a74b0a47b6f89d7
Kashyap <kkas@softwareag.com>
2020-11-10 19:27:02 +0530

    Updated server code. Updated home. Added examples.

---
+++ webapps/examples/ReactApp/src/sampleApp.js
@@ -0,0 +1,6 @@
+class App extends React.Component {
+    render() {
+        return <div className="welcome-text">Sample React App!</div>
+    }
+}
+ReactDOM.render(<App />, document.getElementById('root'));
